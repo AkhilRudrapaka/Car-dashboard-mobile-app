11ce3befa3dfb8230ec4187724308c31149fb4eb
Rudrapaka Akhil <rudrapakaakhil@gmail.com>
2026-03-05 00:26:43 +0530

    Add Vercel deployment config

---
+++ vercel-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 1. Clone Flutter (Stable channel)
+echo "Cloning Flutter SDK..."
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+
+# 2. Add Flutter to PATH
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# 3. Enable Web support
+flutter config --enable-web
+
+# 4. Build the Web App
+echo "Building Flutter Web..."
+flutter build web --release \
+  --dart-define=GOOGLE_MAPS_API_KEY=$GOOGLE_MAPS_API_KEY \
+  --dart-define=FIREBASE_API_KEY=$FIREBASE_API_KEY \
+  --dart-define=FIREBASE_PROJECT_ID=$FIREBASE_PROJECT_ID \
+  --dart-define=FIREBASE_AUTH_DOMAIN=$FIREBASE_AUTH_DOMAIN \
+  --dart-define=FIREBASE_STORAGE_BUCKET=$FIREBASE_STORAGE_BUCKET \
+  --dart-define=FIREBASE_MESSAGING_SENDER_ID=$FIREBASE_MESSAGING_SENDER_ID \
+  --dart-define=FIREBASE_APP_ID=$FIREBASE_APP_ID
